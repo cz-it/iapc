f80d5bf35333e920bcc9967f09a8dd6538e93349
cz-it <cz.theng@gmail.com>
2016-10-30 21:06:38 +0800

    add iOS project

---
+++ include/IAPC.h
@@ -0,0 +1,15 @@
+/*******************************************************************************\
+** IAPC:IAPC.h
+** Created by CZ(cz.devnet@gmail.com) on 10/30/16
+**
+** Copyright © 2016 projm. All rights reserved.
+\*******************************************************************************/
+
+#ifndef IAPC_IAPC_h_
+#define IAPC_IAPC_h_
+
+namespace iapc {
+
+}
+
+#endif /* IAPC_IAPC_h_ */
